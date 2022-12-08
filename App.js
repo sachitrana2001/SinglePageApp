149595b109f5e6ba081e2d5a2876d0654bd5134d
Sachit Rana <105099138+sachitrana2001@users.noreply.github.com>
2022-12-08 15:16:21 +0530

    Basic routing complete

---
+++ App.js
@@ -0,0 +1,31 @@
+import { Route, Switch, Redirect } from "react-router-dom";
+import Welcome from "./Pages/Welcome";
+import Products from "./Pages/Products";
+import MainHeader from "./components/MainHeader";
+import ProductDetail from "./Pages/ProductDetail";
+
+function App() {
+  return (
+    <div>
+      <MainHeader />
+      <main>
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/welcome"/>
+          </Route>
+          <Route path="/welcome">
+            <Welcome />
+          </Route>
+          <Route path="/products" exact>
+            <Products />
+          </Route>
+          <Route path="/products/:productId">
+            <ProductDetail />
+          </Route>
+        </Switch>
+      </main>
+    </div>
+  );
+}
+
+export default App;
